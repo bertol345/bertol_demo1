c4ddcca000d8a9cc6c5cf90d1a3949e685acd530
bertol345 <bertolbrecht7@gmail.com>
2024-10-14 18:33:34 -0500

    Update chat-bot.js

--- JS/chat-bot.js
+++ JS/chat-bot.js
@@ -1,9 +1,1 @@
-const chatToggleBtn = document.getElementById('chat-toggle-btn');
 
-// Mostrar/Ocultar el widget de Tidio al hacer clic
-chatToggleBtn.addEventListener('click', () => {
-    const tidioChat = document.querySelector('iframe[src*="tidio"]');
-    if (tidioChat) {
-        tidioChat.style.display = tidioChat.style.display === 'none' ? 'block' : 'none';
-    }
-});
